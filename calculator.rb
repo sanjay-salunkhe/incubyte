df9cfaee7d70b6cd3b655d1c1ddd08d416a50772
sanjay salunkhe <sanjay.salunkhe@aylanetworks.com>
2025-03-10 16:45:25 +0530

    code to handle different delimiters

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,15 @@
 class Calculator
-  def self.add(arguments)
-    arguments.split(/[\n,]/).map(&:to_i).sum
+  def self.add(input_string)
+    if input_string.start_with?('//')
+      # Extract the delimiter from the first line
+      delimiter = input_string[2..input_string.index("\n") - 1]
+      # Remove the delimiter line from the input string
+      numbers = input_string.split("\n", 2)[1].gsub("\n", delimiter)
+      # Split the numbers by the custom delimiter and sum them
+      numbers.split(delimiter).map(&:to_i).sum
+    else
+      # Handle default case (commas and newlines as delimiters)
+      input_string.split(/[\n,]/).map(&:to_i).sum
+    end
   end
 end
