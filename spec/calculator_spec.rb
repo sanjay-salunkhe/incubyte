19136a1c42f1e25c8940b968b3d497f629a8f7ac
sanjay salunkhe <sanjay.salunkhe@aylanetworks.com>
2025-03-10 15:57:52 +0530

    added more specs

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,8 +1,20 @@
 require 'rspec'
-require_relative '../calculator.rb'
+require_relative '../calculator'
 RSpec.describe Calculator do
-  it 'is expected to return sum of all numbers' do
-    sum = Calculator.add("0")
-    expect(sum).to eq(0)
+  describe '.add' do
+    it 'is expected to return sum of all numbers' do
+      sum = Calculator.add('0')
+      expect(sum).to eq(0)
+    end
+
+    it 'is expected to return 0 when string is empty' do
+      sum = Calculator.add('')
+      expect(sum).to eq(0)
+    end
+
+    it 'is expected to return sum of all numbers' do
+      sum = Calculator.add('1,2,3,4,5,9')
+      expect(sum).to eq(24)
+    end
   end
 end
