e71c1117ded10f454900a9a4e27c6736ba3f9f7e
sanjay salunkhe <sanjay.salunkhe@aylanetworks.com>
2025-03-10 16:51:43 +0530

    added test cases for negative numbers

--- calculator.rb
+++ calculator.rb
@@ -5,11 +5,25 @@ class Calculator
       delimiter = input_string[2..input_string.index("\n") - 1]
       # Remove the delimiter line from the input string
       numbers = input_string.split("\n", 2)[1].gsub("\n", delimiter)
-      # Split the numbers by the custom delimiter and sum them
-      numbers.split(delimiter).map(&:to_i).sum
+      # Split the numbers by the custom delimiter and convert them to integers
+      numbers_array = numbers.split(delimiter).map(&:to_i)
+
+      # Check for negative numbers and raise an exception if any are found
+      negatives = numbers_array.select { |num| num < 0 }
+      raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+      # Return the sum of the numbers
+      numbers_array.sum
     else
       # Handle default case (commas and newlines as delimiters)
-      input_string.split(/[\n,]/).map(&:to_i).sum
+      numbers_array = input_string.split(/[\n,]/).map(&:to_i)
+
+      # Check for negative numbers and raise an exception if any are found
+      negatives = numbers_array.select { |num| num < 0 }
+      raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+      # Return the sum of the numbers
+      numbers_array.sum
     end
   end
 end
