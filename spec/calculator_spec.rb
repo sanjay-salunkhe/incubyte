2d6b94edf8850421a3532d85edd806cbf393d03e
sanjay salunkhe <sanjay.salunkhe@aylanetworks.com>
2025-03-10 15:51:06 +0530

    test case to add numbers seperated by comma

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,4 +1,8 @@
 require 'rspec'
 require_relative '../calculator.rb'
 RSpec.describe Calculator do
+  it 'is expected to return sum of all numbers' do
+    sum = Calculator.add("0")
+    expect(sum).to eq(0)
+  end
 end
