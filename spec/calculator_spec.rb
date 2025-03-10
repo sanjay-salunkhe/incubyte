8f33a46ef450e51d507034503ee8bc03370c935b
sanjay salunkhe <sanjay.salunkhe@aylanetworks.com>
2025-03-10 16:19:30 +0530

    addded spec to handle new lines

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -16,5 +16,22 @@ RSpec.describe Calculator do
       sum = Calculator.add('1,2,3,4,5,9')
       expect(sum).to eq(24)
     end
+
+    it 'returns the sum of numbers separated by newlines' do
+      expect(Calculator.add("1\n2\n3")).to eq(6)
+      expect(Calculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'returns the sum of numbers separated by both commas and newlines' do
+      expect(Calculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'handles multiple delimiters' do
+      expect(Calculator.add("1,2\n3,4\n5")).to eq(15)
+    end
+
+    it 'returns 0 for a string with only delimiters' do
+      expect(Calculator.add("\n,\n")).to eq(0)
+    end
   end
 end
